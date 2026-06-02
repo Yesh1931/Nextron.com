f122f9b05a1ce369088bffd1b6d54b1397f32c1d
Yeshwa <ch.yeshwanth90@gmail.com>
2026-06-02 23:31:40 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -7,7 +7,7 @@
 
 // Replace these placeholders with your actual Firebase project settings
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "./firebase/app";
+import { initializeApp } from "./firebase/app.js";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
