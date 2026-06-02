fea03092566fd5e84e6ce8f37bea2a26b9df475d
Yeshwa <ch.yeshwanth90@gmail.com>
2026-06-03 00:02:08 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -7,9 +7,14 @@
 
 // Replace these placeholders with your actual Firebase project settings
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
